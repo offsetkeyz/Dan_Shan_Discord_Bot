bacb9aeae76caff63d8d37cf1a2ac5adbf6de3b5
Colin <colin.mcallister89@yahoo.com>
2022-09-14 17:01:19 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,12 +1,14 @@
 from cmath import log
 import json
 import sys
+import traceback
 from discord.ext import tasks, commands
 from discord.ext.commands import Context
 import random
 import discord
 import discord.ext
 import os
+import platform
 import asyncio
 from discord.ext import commands
 
@@ -52,8 +54,64 @@ async def on_message(message: discord.Message) -> None:
         print('error test line 76 bot.py')
 
 @bot.event
+async def on_command_error(self, ctx, error):
+        """The event triggered when an error is raised while invoking a command.
+        Parameters
+        ------------
+        ctx: commands.Context
+            The context used for command invocation.
+        error: commands.CommandError
+            The Exception raised.
+        """
+        # This prevents any commands with local handlers being handled here in on_command_error.
+        if hasattr(ctx.command, 'on_error'):
+            return
+
+        # This prevents any cogs with an overwritten cog_command_error being handled here.
+        cog = ctx.cog
+        if cog:
+            if cog._get_overridden_method(cog.cog_command_error) is not None:
+                return
+
+        ignored = (commands.CommandNotFound, )
+
+        # Allows us to check for original exceptions raised and sent to CommandInvokeError.
+        # If nothing is found. We keep the exception passed to on_command_error.
+        error = getattr(error, 'original', error)
+
+        # Anything in ignored will return and prevent anything happening.
+        if isinstance(error, ignored):
+            return
+
+        if isinstance(error, commands.DisabledCommand):
+            await ctx.send(f'{ctx.command} has been disabled.')
+
+        elif isinstance(error, commands.NoPrivateMessage):
+            try:
+                await ctx.author.send(f'{ctx.command} can not be used in Private Messages.')
+            except discord.HTTPException:
+                pass
+
+        # For this error example we check to see where it came from...
+        elif isinstance(error, commands.BadArgument):
+            if ctx.command.qualified_name == 'tag list':  # Check if the command being invoked is 'tag list'
+                await ctx.send('I could not find that member. Please try again.')
+
+        else:
+            # All other Errors not returned come here. And we can just print the default TraceBack.
+            print('Ignoring exception in command {}:'.format(ctx.command), file=sys.stderr)
+            traceback.print_exception(type(error), error, error.__traceback__, file=sys.stderr)
+
+@bot.event
 async def on_ready():
-    print('Hello!')
+    """
+    The code in this even is executed when the bot is ready
+    """
+    print(f"Logged in as {bot.user.name}")
+    print(f"discord.py API version: {discord.__version__}")
+    print(f"Python version: {platform.python_version()}")
+    print(f"Running on: {platform.system()} {platform.release()} ({os.name})")
+    print("-------------------")
     status_task.start()
     cogs_reload.start()
 
