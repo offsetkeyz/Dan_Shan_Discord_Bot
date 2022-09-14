ebda986dc581e789f22c7bf3b3c982dc76d6d5c2
Colin <colin.mcallister89@yahoo.com>
2022-09-14 16:07:25 -0400

    fixed commands

--- cogs/basic_commands.py
+++ cogs/basic_commands.py
@@ -12,16 +12,27 @@ async def saymore(ctx, *, message=None):
     await ctx.send(f'{message}')
 
 @commands.command(
-        # name="help",
-        # description="List all commands the bot has loaded."
+        name="help",
+        description="List all commands the bot has loaded."
     )
-async def helpp(self, context: Context) -> None:
-    print('pp')
+async def help(self, context: Context) -> None:
+    prefix = self.bot.config["prefix"]
+    embed = discord.Embed(title="Help", description="List of available commands:", color=0x9C84EF)
+    for i in self.bot.cogs:
+        cog = self.bot.get_cog(i.lower())
+        commands = cog.get_commands()
+        data = []
+        for command in commands:
+            description = command.description.partition('\n')[0]
+            data.append(f"{prefix}{command.name} - {description}")
+        help_text = "\n".join(data)
+        embed.add_field(name=i.capitalize(), value=f'```{help_text}```', inline=False)
+    await context.send(embed=embed)
 
 # async 
 def setup(bot):
     # await 
-    commands = ['helpp','saymore']
+    commands = [help,saymore]
     for command in commands:
         bot.add_command(command)
 
