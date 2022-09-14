762b6b8b055c10bebf49b883207c915b966ab175
Colin <colin.mcallister89@yahoo.com>
2022-09-13 21:17:13 -0400

    fix stuffs

--- bot.py
+++ bot.py
@@ -1,47 +1,36 @@
-import decouple as decouple
+import json
+import sys
+from discord.ext import tasks, commands
+import random
 import discord
 import discord.ext
 import os
-from b import DSC_TOKEN
-
+import asyncio
 from discord.ext import commands
 from nslookup import Nslookup
 
-client = commands.Bot(command_prefix=["!", "?", "%"])
-domain = "pipi-pupu.sytes.net"
-dns_query = Nslookup()
+if not os.path.isfile("config.json"):
+    sys.exit("'config.json' not found! Please add it and try again.")
+else:
+    with open("config.json") as file:
+        config = json.load(file)
 
-# TODO REMOVE TOKEN BEFORE GITHUB
-TOKEN = DSC_TOKEN
+client = commands.Bot(command_prefix=config['prefix'])
 
+TOKEN = config['token']
+
+@tasks.loop(minutes=1.0)
+async def status_task() -> None:
+    """
+    Setup the game status task of the bot
+    """
+    statuses = ["with screagles!", "with terpic lertning", "with not sleggert"]
+    await client.change_presence(activity=discord.Game(random.choice(statuses)))
 
 @client.event
 async def on_ready():
     print('Hello!')
-
-
-@client.command()
-async def gametime(ctx):
-    await ctx.send('Game time: 6:30pm PST')
-
-
-# Valheim Server
-@client.command()
-async def vserver(ctx):
-    ips_record = dns_query.dns_lookup(domain)
-    server = str(ips_record.response_full) + " " + str(ips_record.answer)
-    await ctx.send("The current IP for our glorious server is " + str(ips_record.answer[0]) + ":2456")
-
-
-@client.command()
-async def beans(ctx):
-    await ctx.send(':exploding_head: fuk ur beenz')
-
-
-@client.command()
-async def ping(ctx):
-    await ctx.send(':ping_pong: Pong!')
-
+    status_task.start()
 
 @client.command()
 # '*' is to include everything after command
@@ -52,11 +41,9 @@ async def say(ctx, *, message=None):
         return
     await ctx.send(f'{message}')
 
-
 @client.command()
 async def userinfo(ctx, user: discord.User):
     await ctx.send(user.display_name + " has earned their place in this clan. You know how...")
-
 
 # Multiple Arguments
 @client.command()
@@ -64,5 +51,21 @@ async def multi(ctx, role: discord.Role, user: discord.User):
     await ctx.send(role.id)
     await ctx.send(user.id)
 
+async def load_cogs() -> None:
+    """
+    The code in this function is executed whenever the bot will start.
+    """
+    for file in os.listdir(f"./cogs"):
+        if file.endswith(".py"):
+            extension = file[:-3]
+            try:
+                await client.load_extension(f"cogs.{extension}")
+                print(f"Loaded extension '{extension}'")
+            except Exception as e:
+                exception = f"{type(e).__name__}: {e}"
+                print(f"Failed to load extension {extension}\n{exception}")
 
+
+
+asyncio.run(load_cogs())
 client.run(TOKEN)
--- cogs/events.py
+++
@@ -1,7 +0,0 @@
-import discord
-from discord.ext import commands
-
-class Events(commands.Cog):
-
-    def __init__(self, client):
-        self.client = client
