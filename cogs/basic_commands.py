0dfb3adfbd25bfd7ee6d938e4e5427314355f4ce
Colin <colin.mcallister89@yahoo.com>
2022-09-14 15:47:31 -0400

    Update basic_commands.py

--- cogs/basic_commands.py
+++ cogs/basic_commands.py
@@ -1,24 +1,16 @@
-
 from discord.ext import commands
  
-class BasicCommands(commands.Cog):
-    def __init__(self, bot):
-        self.bot = bot
-
-    @commands.command()
-    # '*' is to include everything after command
-    # Consume all argument must be last in the function
-    # async 
-    def saymore(ctx, *, message=None):
-        if message is None:
-            # await 
-            ctx.send('Please provide a message')
-            return
-        # await 
-        ctx.send(f'{message}')
+@commands.command()
+# '*' is to include everything after command
+# Consume all argument must be last in the function
+async def saymore(ctx, *, message=None):
+    if message is None:
+        await ctx.send('Please provide a message')
+        return
+    await ctx.send(f'{message}')
 
 # async 
 def setup(bot):
     # await 
-    bot.add_cog(BasicCommands(bot))
+    bot.add_command(saymore)
 
