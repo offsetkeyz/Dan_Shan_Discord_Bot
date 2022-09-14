4f34ddd39ab2ea2d6ffaf98bdf1717b59fff43c3
Colin <colin.mcallister89@yahoo.com>
2022-09-14 16:32:38 -0400

    Update basic_commands.py

--- cogs/basic_commands.py
+++ cogs/basic_commands.py
@@ -5,11 +5,16 @@ from discord.ext.commands import Context
 @commands.command()
 # '*' is to include everything after command
 # Consume all argument must be last in the function
-async def saymore(ctx, *, message=None):
+async def say(ctx, *, message=None):
     if message is None:
         await ctx.send('Please provide a message')
         return
-    await ctx.send(f'{message}')
+    embed = discord.Embed(
+            title="Hey, you think you can tell me what to say??",
+            description=f"Ok will this make you happy? Dan Shan has been known to say {message}",
+            color=0xE02B2B
+        )
+    await ctx.send(embed=embed)
 
 @commands.command(
     name="userinfo",
@@ -21,7 +26,7 @@ async def userinfo(ctx, user: discord.User):
 # async 
 def setup(bot):
     # await 
-    commands = [userinfo,saymore]
+    commands = [userinfo,say]
     for command in commands:
         bot.add_command(command)
 
