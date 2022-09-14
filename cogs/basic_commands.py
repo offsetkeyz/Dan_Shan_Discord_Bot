9402b36454c1eea2c1a0a2e75a4efb774cd32c61
Colin <colin.mcallister89@yahoo.com>
2022-09-14 15:39:47 -0400

    Update basic_commands.py

--- cogs/basic_commands.py
+++ cogs/basic_commands.py
@@ -5,14 +5,17 @@ class BasicCommands(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.hybrid_command()
+    @commands.command()
     # '*' is to include everything after command
     # Consume all argument must be last in the function
-    async def saymore(ctx, *, message=None):
+    # async 
+    def saymore(ctx, *, message=None):
         if message is None:
-            await ctx.send('Please provide a message')
+            # await 
+            ctx.send('Please provide a message')
             return
-        await ctx.send(f'{message}')
+        # await 
+        ctx.send(f'{message}')
 
 # async 
 def setup(bot):
